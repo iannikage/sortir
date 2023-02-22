92b46a6df4092dc8419947cab760c1953771adb8
sarahdoz <sarah.dozeville@aol.com>
2023-02-22 11:04:55 +0100

    Mise en place S'inscrire + se connecter

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Participant;
+use App\Form\RegistrationFormType;
+use App\Security\AppAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $participantPasswordHasher,
+        UserAuthenticatorInterface $participantAuthenticator,
+        AppAuthenticator $authenticator,
+        EntityManagerInterface $entityManager): Response
+    {
+        $participant = new Participant();
+        $form = $this->createForm(RegistrationFormType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $participant->setPassword(
+            $participantPasswordHasher->hashPassword(
+                    $participant,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($participant);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $participantAuthenticator->authenticateUser(
+                $participant,
+                $authenticator,
+                $request
+            );
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
