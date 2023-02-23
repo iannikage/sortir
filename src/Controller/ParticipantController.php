2c9f04cd6758afcd3891262cd03365b1fec4c86f
iannikage <yann.lesieur@free.fr>
2023-02-23 10:54:05 +0100

    Création ProfilFormType Controller ModifierProfil Et la page twig monprofil.html.twig ParticipantController.php
    Permet d'afficher le profil dans un formulaire et de modifier les infos

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Participant;
+use App\Form\ProfilFormType;
 use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,14 +16,28 @@ class ParticipantController extends AbstractController
     /**
      * @Route("/monprofil", name="monprofil")
      */
-    public function afficherProfil(ParticipantRepository $participantRepository): Response
+    public function modifierProfil(ParticipantRepository $participantRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
         /** @var Participant $participant */
         $participant = $this->getUser();
-        return $this->render('participant/monprofil.html.twig',[
-            'participant' => $participant,
+        $profilForm = $this->createForm(ProfilFormType::class, $participant);
+        $profilForm->handleRequest($request);
+
+        if ($profilForm->isSubmitted() && $profilForm->isValid()) {
+            $entityManager->persist($participant);
+            $entityManager->flush();
+
+
+            $this->addFlash('success','Profil modifié avec succès');
+            return $this->redirectToRoute('monprofil');
+        }
+
+
+
+        return $this->render('participant/monprofil.html.twig', [
+            'ProfilFormType' => $profilForm->createView()
         ]);
 
-    }
+}
 
 }
