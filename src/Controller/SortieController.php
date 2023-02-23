55a714df1a830c278d04edf5d4ed15dc63233784
sarahdoz <sarah.dozeville@aol.com>
2023-02-23 11:59:53 +0100

    Sortie->controller/twigs

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -6,15 +6,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/sortie", name="app_sortie_")
+ */
 class SortieController extends AbstractController
 {
     /**
-     * @Route("/sortie", name="app_sortie")
+     * @Route("/", name="create")
      */
-    public function index(): Response
+    public function createSortie(): Response
     {
-        return $this->render('sortie/index.html.twig', [
+        return $this->render('sortie/create.html.twig', [
             'controller_name' => 'SortieController',
         ]);
     }
+    /**
+     * @Route("/", name="afficher")
+     */
+    public function afficherSortie(): Response
+    {
+        return $this->render('sortie/afficher.html.twig', [
+            'controller_name' => 'SortieController',
+        ]);
+    }
+    /**
+     * @Route("/", name="annuler")
+     */
+    public function annulerSortie(): Response
+    {
+        return $this->render('sortie/annuler.html.twig', [
+            'controller_name' => 'SortieController',
+        ]);
+    }
+
+
 }
